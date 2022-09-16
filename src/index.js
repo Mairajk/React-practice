116fad4d2a34bc891077a009019512bac02d2721
Mairajk <mairajkhan597@gmail.com>
2022-09-16 17:16:59 +0500

    nav bar working....1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,181 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import moment from 'moment'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import pP1 from './images/pP1.jpg'
+import pP2 from './images/pP2.jpg'
+import pP3 from './images/pP3.jpg'
+import pP4 from './images/pP4.jpg'
+import pP5 from './images/pP5.jpg'
+import pP6 from './images/pP6.jpg'
+import pP7 from './images/pP7.jpg'
+
+import pI1 from './images/pI1.jpg'
+import pI2 from './images/pI2.jpg'
+import pI3 from './images/pI3.jpg'
+import pI4 from './images/pI4.jpg'
+import pI5 from './images/pI5.jpg'
+import pI6 from './images/pI6.jpg'
+import pI7 from './images/pI7.jpg'
+
+import { TbThumbUp, TbGridDots } from "react-icons/tb";
+import { RiShareForwardLine, RiFacebookCircleFill, RiMessengerFill } from "react-icons/ri";
+import { VscComment, VscHome } from "react-icons/vsc";
+import { BiSearchAlt2 } from "react-icons/bi";
+// import { TiHome } from "react-icons/ti";
+import { BsFillPeopleFill, BsFillGrid3X3GapFill, BsFillBellFill } from "react-icons/bs";
+import { MdOutlineOndemandVideo } from "react-icons/md";
+import { HiUserGroup } from "react-icons/hi";
+import { SiFacebookgaming } from "react-icons/si";
+
+//==============================================================================================
+//==============================================================================================
+//==============================================================================================
+
+let Nav = () => {
+  return (
+    <div className='navBar'>
+      <div className='left'>
+        <div>
+          <i className="facebookLogo leftLogo navLogo"><RiFacebookCircleFill /></i>
+        </div>
+        <div className='inputDiv'>
+          <label htmlFor="search">
+            <i className="searchLogo leftLogo navLogo"><BiSearchAlt2 /></i>
+          </label>
+          <input type="search" placeholder='Search Facebook' />
+        </div>
+      </div>
+
+      <div className="center">
+        {/* <div className='homeLogo'><i><TiHome/></i></div> */}
+        <div><i className='homeLogo centerLogo navLogo'><VscHome /></i></div>
+        <div><i className='friendsLogo centerLogo navLogo'><BsFillPeopleFill /></i></div>
+        <div><i className='watchLogo centerLogo navLogo'><MdOutlineOndemandVideo /></i></div>
+        <div><i className='groupsLogo centerLogo navLogo'><HiUserGroup /></i></div>
+        <div><i className='gamingLogo centerLogo navLogo'><SiFacebookgaming /></i></div>
+      </div>
+
+
+      <div className="right">
+        <div><i className="menuLogo rightLogo navLogo"><TbGridDots /></i></div>
+        {/* <div className="menuLogo rightLogo navLogo"><i><BsFillGrid3X3GapFill /></i></div> */}
+        <div><i className="messengerLogo rightLogo navLogo"><RiMessengerFill /></i></div>
+        <div><i className="bellLogo rightLogo navLogo"><BsFillBellFill /></i></div>
+        {/* <div> <img className="navProfilePhoto rightLogo navLogo" src="" alt="" /></div> */}
+      </div>
+
+    </div>
+  )
+}
+
+
+
+//=============================================================================
+
+let Post = (props) => {
+
+  return <div className='post'>
+
+    <div className="postHead">
+      <img src={props.profilePhoto} alt="profilePhoto" className='profilePhoto' />
+      <div className="nameDate">
+        <p className='userName'>{props.userName}</p>
+        <p className='postDate'>{moment(props.postDate).fromNow()}</p>
+      </div>
+    </div>
+
+    <div className="postText">{props.postText}</div>
+
+    <div className="postImage" >
+      <img src={props.postImage} alt="" />
+    </div>
+
+    <div className='postFooter'>
+      {/* <div> <FontAwesomeIcon icon={faThumbsUp} /> Like </div>
+      <div> <FontAwesomeIcon icon={faComment} /> Comment</div>
+      <div> <FontAwesomeIcon icon={faShare} /> Share</div> */}
+      <div><i><TbThumbUp /></i><span className='iconText'>Like</span></div>
+      <div><i><VscComment /></i><span className='iconText'>Comment</span></div>
+      <div><i><RiShareForwardLine /></i><span className='iconText'>Share</span></div>
+
+
+    </div>
+
+  </div>
+};
+
+let Page = () => {
+
+
+  return (
+    <div className="page">
+
+      <h1> Facebook</h1>
+
+      <Nav />
+
+      <Post
+        userName="Alisa Henry"
+        profilePhoto={pP1}
+        postDate="21 Dec 2020"
+        postText="Sunset does not darken the world, but it gives us hope of another bright day.
+        It's we who lose hope and push ourselves into darkness."
+        postImage={pI1}
+      />
+
+      <Post
+        userName="William Roger"
+        profilePhoto={pP2}
+        postDate="11 Jan 2022"
+        postText="What a peaceful view."
+        postImage={pI2}
+      />
+
+      <Post
+        userName="Alexendra Wilson"
+        profilePhoto={pP3}
+        postDate="8 Mar 2022"
+        postText="New winter collection of 2022 coming soon."
+        postImage={pI3}
+      />
+
+      <Post
+        userName="Smith Dolton"
+        profilePhoto={pP4}
+        postDate="15 Jun 2022"
+        postText="Art of Nature."
+        postImage={pI4}
+      />
+
+      <Post
+        userName=" Chiristopher Nicholas "
+        profilePhoto={pP5}
+        postDate="04 Aug 2022"
+        // postText="When React renders a component, it passes the component’s props (short for “properties”) as the first argument, as an object."
+        postImage={pI5}
+      />
+
+      <Post
+        userName="George Vince"
+        profilePhoto={pP6}
+        postDate="09 Oct 2021"
+        postText="Nothing is beautiful than the beauty of nature, specially in rainy season."
+        postImage={pI6}
+      />
+
+      <Post
+        userName="Alisa Hank"
+        profilePhoto={pP7}
+        postDate="25 Oct 2021"
+        postText="Let's go Dragoon !"
+        postImage={pI7}
+      />
+
+    </div>
+  )
+};
+
+ReactDOM.render(<Page />, document.querySelector('#root'));
